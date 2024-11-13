3b3367418dd307be700b08eb1d5223cff8cfa7a9
Sobreira <matheus.paixao.ssobreira@gmail.com>
2024-11-13 19:50:25 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+ 
+var builder = WebApplication.CreateBuilder(args);
+ 
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+ 
+var app = builder.Build();
+ 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+ 
+app.UseHttpsRedirection();
+ 
 // Define o endpoint para SNMP walk
 app.MapGet("/snmpwalk/{versao}/{community}/{enderecoIP}", async context =>
 {
-    context.Response.ContentType = "text/plain"; // Define o tipo de conteúdo como texto simples
-    context.Response.Headers["Content-Disposition"] = "inline"; // Adiciona cabeçalho para exibição inline
-
+    context.Response.ContentType = "text/plain";
+    context.Response.Headers["Content-Disposition"] = "inline";
     if (context.Request.RouteValues.TryGetValue("versao", out var versao) &&
         context.Request.RouteValues.TryGetValue("community", out var community) &&
         context.Request.RouteValues.TryGetValue("enderecoIP", out var enderecoIP))
@@ -17,7 +38,7 @@ app.MapGet("/snmpwalk/{versao}/{community}/{enderecoIP}", async context =>
             RedirectStandardOutput = true,
             RedirectStandardError = true
         };
-
+ 
         using (var cmd = Process.Start(process))
         {
             if (cmd == null)
@@ -25,18 +46,14 @@ app.MapGet("/snmpwalk/{versao}/{community}/{enderecoIP}", async context =>
                 await context.Response.WriteAsync("Falha ao iniciar o processo.");
                 return;
             }
-
             await cmd.WaitForExitAsync();
-
             string output = await cmd.StandardOutput.ReadToEndAsync();
             string error = await cmd.StandardError.ReadToEndAsync();
-
             if (!string.IsNullOrEmpty(error))
             {
                 await context.Response.WriteAsync($"Erro ao executar o script: {error}");
                 return;
             }
-
             string caminhoArquivo = "/tmp/saida.txt";
             if (File.Exists(caminhoArquivo))
             {
@@ -57,3 +74,5 @@ app.MapGet("/snmpwalk/{versao}/{community}/{enderecoIP}", async context =>
         await context.Response.WriteAsync("Parâmetros inválidos.");
     }
 });
+ 
+app.Run();

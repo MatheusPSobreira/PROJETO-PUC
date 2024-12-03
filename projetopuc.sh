300c0adef0042eb7dcfd6bbaa02f633fbd8c01ec
Sobreira <matheus.paixao.ssobreira@gmail.com>
2024-12-03 17:17:03 -0300

    Update projetopuc.sh

--- projetopuc.sh
+++ projetopuc.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Verifique se os parâmetros foram passados corretamente
 if [ "$#" -ne 3 ]; then
     echo "Uso: $0 <versao> <community> <enderecoIP>"
     exit 1
@@ -14,11 +12,9 @@ echo "Versão: $VERSAO"
 echo "Community: $COMMUNITY"
 echo "Endereço IP: $ENDERECO_IP"
 
-# Executar o comando SNMP
 echo "Executando o comando SNMP..."
 snmpwalk -v "$VERSAO" -c "$COMMUNITY" "$ENDERECO_IP" > /tmp/saida.txt 2>&1
 
-# Verifique se o comando SNMP executou corretamente
 if [ $? -ne 0 ]; then
     echo "Erro ao executar o comando SNMP. Verifique /tmp/saida.txt para detalhes."
     exit 1
